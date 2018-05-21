8ffcf5fdc6e9dac139e70d08cfafd5a77397522b
Unknown <felix@felixsoum.com>
2018-05-21 02:00:38 -0400

    Add human wander

---
+++ Assets/Scripts/Human.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Human : MonoBehaviour
+{
+    new Rigidbody2D rigidbody;
+    bool isDragged;
+    float wanderTimeMin = 1;
+    float wanderTimeMax = 5;
+    float wanderForce = 2;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        Invoke("Wander", Random.Range(wanderTimeMin, wanderTimeMax));
+    }
+
+	void OnMouseDown()
+    {
+        isDragged = true;
+        rigidbody.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isDragged = false;
+        rigidbody.isKinematic = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (isDragged)
+        {
+            Vector3 pos = Util.ClampToWorld(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            pos.z = transform.position.z;
+            transform.position = pos;
+        }
+    }
+
+    void Wander()
+    {
+        if (!isDragged)
+        {
+            rigidbody.AddForce(Random.insideUnitCircle * wanderForce, ForceMode2D.Impulse);
+        }
+        Invoke("Wander", Random.Range(wanderTimeMin, wanderTimeMax));
+    }
+
+}
---
+++ Assets/Scripts/Util.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public static Vector3 ClampToWorld(Vector3 v)
+    {
+        v.x = Mathf.Clamp(v.x, -2.75f, 2.75f);
+        v.y = Mathf.Clamp(v.y, -4.5f, 4.5f);
+        return v;
+    }
+}
