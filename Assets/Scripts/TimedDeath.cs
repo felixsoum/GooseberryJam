8ed79376274bf4125b32c25c01caeb47330c52dc
Unknown <felix@felixsoum.com>
2018-05-27 15:34:30 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/TimedDeath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedDeath : MonoBehaviour
+{
+    public float time;
+
+    void Start()
+    {
+        Invoke("Die", time);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
